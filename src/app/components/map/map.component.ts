a1f498063ba2961ba949ef07b13f3ab2f71910ba
manon-bernard <manon.bernard8907@gmail.com>
2023-10-17 14:12:56 +0200

    Feature: Added ol map in map component

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
+export class MapComponent implements OnInit{
+
+  map: Map;
+
+  constructor(){
+    this.map = new Map();
+  }
+
+  ngOnInit(): void {
+    this.map = new Map({
+        view: new View({
+          center: [0,0],
+          zoom: 1,
+        }),
+        layers:[
+          new TileLayer({
+            source: new OSM(),
+          })
+        ],
+        target:'ol-map',
+      }
+    )
+  }
 
 }
