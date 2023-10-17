36cdc82b8174166bf677de4bd3b70dbc427d59f7
manon-bernard <manon.bernard8907@gmail.com>
2023-10-17 17:08:18 +0200

    Feature: Added Geolocation on map + user marker position

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -3,6 +3,14 @@ import Map from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import OSM from 'ol/source/OSM';
+import Geolocation from 'ol/Geolocation.js';
+import { Coordinate } from 'ol/coordinate';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
+import {Circle, Fill, Stroke, Style} from 'ol/style.js';
+import {useGeographic} from 'ol/proj.js';
 
 @Component({
   selector: 'app-map',
@@ -11,26 +19,93 @@ import OSM from 'ol/source/OSM';
 })
 export class MapComponent implements OnInit{
 
-  map: Map;
-
-  constructor(){
-    this.map = new Map();
-  }
+  map = new Map();
+  userLocation: Coordinate = [2.349014, 48.864716] // Paris Default
+  userMarkerLayer = new VectorLayer();
+  geolocation = new Geolocation({
+    tracking:true,
+    trackingOptions: {
+      enableHighAccuracy: true,
+    },
+  })
 
   ngOnInit(): void {
-    this.map = new Map({
-        view: new View({
-          center: [0,0],
-          zoom: 1,
-        }),
-        layers:[
-          new TileLayer({
-            source: new OSM(),
-          })
-        ],
-        target:'ol-map',
-      }
-    )
+    useGeographic(); // To use coordinates.
+    console.log(this.userLocation)
+    this.createMap();
+    this.updateUserLocation();
+    console.log(this.userLocation)
   }
+
+  createMap(): void {
+    console.log('init')
+    this.map = new Map({
+      target:'ol-map',
+      view: new View({
+        center: this.userLocation,
+        zoom: 13,
+      }),
+      layers:[
+        new TileLayer({
+          source: new OSM(),
+        })
+      ]
+    })
+  }
+
+  updateMapCenter(coordinates: Coordinate):void{
+    this.map.getView().setCenter(coordinates);
+  }
+
+  updateUserLocation() {
+
+    this.geolocation.on('change:position', () => {
+      const userLocation: Coordinate | undefined = this.geolocation.getPosition();
+      console.log('test', userLocation);
+
+      if (userLocation) {
+        console.log('user location changed', userLocation);
+        this.userLocation = userLocation;
+        this.updateMapCenter(this.userLocation);
+        this.updateUserMarker(this.userLocation)
+      } else {
+        console.log('Geolocation not available or permission denied. Centering on Paris.');
+        this.userLocation = [2.349014, 48.864716]; // Paris coordinates
+        this.updateMapCenter(this.userLocation);
+      }
+
+    });
+  }
+
+    updateUserMarker(coordinates: Coordinate) {
+      console.log(coordinates)
+      if (this.userMarkerLayer) {
+      this.map.removeLayer(this.userMarkerLayer);
+      }
+
+      const userMarkerLayer = new VectorLayer({
+        source: new VectorSource({
+          features: [
+            new Feature({
+              geometry: new Point(coordinates)
+            })
+          ]}),
+        style: new Style({
+          image: new Circle({
+            radius: 6,
+            fill: new Fill({
+              color: '#3399CC',
+            }),
+            stroke: new Stroke({
+              color: '#fff',
+              width: 2,
+            }),
+          })
+        })
+      });
+
+      this.userMarkerLayer = userMarkerLayer;
+      this.map.addLayer(this.userMarkerLayer);
+    }
 
 }
